7383dfb1dcbefc9388a0af808df745813d980751
Katie Nink Smith <katiej.smith@outlook.com>
2025-02-25 17:25:41 -0700

    menu data added

---
+++ data/menuItems.js
@@ -0,0 +1,70 @@
+const menu = [
+  {
+    name: "Barvarian Cream",
+    image: "public/barvarianCream.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Frosted Yeast Ring",
+    image: "public/blueFrosting.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Chocolate Yeast Ring",
+    image: "public/chocolateFrosting.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Chocolate Long John",
+    image: "public/chocolateLongJohn.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Maple Long John",
+    image: "public/mapleBar.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Frosted Yeast Ring",
+    image: "public/pinkFrosting.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Raspberry Filled",
+    image: "public/raspberryFilled.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Yeast Twist",
+    image: "public/twist.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "Frosted Yeast Ring",
+    image: "public/whiteFrosting.jpg",
+    type: "Donut",
+    price: "",
+  },
+  {
+    name: "",
+    image: "",
+    type: "",
+    price: "",
+  },
+  {
+    name: "",
+    image: "",
+    type: "",
+    price: "",
+  },
+];
+
+export default menu;
--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import menu from "../../data/menuItems.js";
 
 function Images() {
+  const [currentDonut, setCurrentDonut] = useState(menu[0]?.name);
+  const [currentImage, setCurrentImage] = useState(menu[0]?.image);
+
+  useEffect(() => {
+    const currentImage = menu[currentDonut]?.image;
+    setCurrentImage(currentImage);
+    // onDonutChange(currentDonut, currentImage);
+  }, [currentImage]);
+
   return (
     <div>
-      <img
-        src="./public/dozenDonuts.jpg"
-        width="300px"
-        alt="a variety of donuts"
-      />
+      <div className="image">
+        <h2
+          style={{
+            fontSize: "500%",
+            textShadow: "1px 1px white",
+            marginTop: "10px",
+            marginBottom: "250px",
+          }}
+        >
+          {menu[currentDonut]?.name}
+        </h2>
+      </div>
     </div>
   );
 }
